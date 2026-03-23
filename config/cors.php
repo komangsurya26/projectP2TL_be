23e2161c88c4d0117135aa67ea72632bf967a885
Komang Surya <komangsuryasedana@gmail.com>
2026-03-23 10:18:00 +0800

    feat: Configure CORS to include sanctum/csrf-cookie path, allow all methods, and set max_age to 0, while updating the deploy workflow to clear config, application, and route caches.

--- config/cors.php
+++ config/cors.php
@@ -1,11 +1,11 @@
 <?php
 return [
-    'paths' => ['api/*'],
-    'allowed_methods' => ['POST', 'GET', 'OPTIONS', 'PUT', 'DELETE'],
-    'allowed_origins' => [env('APP_URL_FRONTEND', 'https://p2tlanalisa.web.id')],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => ['https://p2tlanalisa.web.id'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
-    'max_age' => 3600,
+    'max_age' => 0,
     'supports_credentials' => false
 ];
